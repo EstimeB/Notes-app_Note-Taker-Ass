49cf2d166ba6eac390337b4abc988beac8b5a09e
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-21 19:20:50 -0700

    Cutted and exported few lines to newly created index.js file under route/html folder. Also created and api folder.

---
+++ route/html/index.js
@@ -0,0 +1,20 @@
+const app = require("express").Router()
+const path = require('path');
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../../public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '../../public/notes.html'))
+);
+
+// Redirecting to home page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../../public/index.html'))
+);
+
+module.exports = app;
